f00167e41b6e0ed86b995b6f83e6d90af05c43e9
Marko Vujošević <42012973+DoubleLane@users.noreply.github.com>
2025-09-09 08:01:12 +0200

    Update Wallet.java

--- src/blockchain/Wallet.java
+++ src/blockchain/Wallet.java
@@ -34,7 +34,7 @@ public class Wallet {
 	//returns balance and stores the UTXO's owned by this wallet in this.UTXOs
 		public float getBalance() {
 			float total = 0;	
-	        for (Map.Entry<String, TransactionOutput> item: Noob.UTXOs.entrySet()){
+	        for (Map.Entry<String, TransactionOutput> item: Chain.UTXOs.entrySet()){
 	        	TransactionOutput UTXO = item.getValue();
 	            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
 	            	UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
